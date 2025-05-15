ff07477248acb40fe2d5ba0d0d38e73972e505af
Reverbgamer <87449746+Reverbgamer@users.noreply.github.com>
2025-05-15 20:10:13 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2697,7 +2697,7 @@ do
 
     local WatermarkOuter = Library:Create('Frame', {
         BorderColor3 = Color3.new(0, 0, 0);
-        Position = UDim2.new(0, 100, 0, -25);
+        Position = UDim2.new(0, 100, 100, -25);
         Size = UDim2.new(0, 213, 0, 20);
         ZIndex = 200;
         Visible = false;
@@ -2745,7 +2745,7 @@ do
     });
 
     local WatermarkLabel = Library:CreateLabel({
-        Position = UDim2.new(0, 2, 0, 0);
+        Position = UDim2.new(0, 6, 0, 0);
         Size = UDim2.new(1, -4, 1, 0);
         TextSize = 14;
         TextXAlignment = Enum.TextXAlignment.Left;
